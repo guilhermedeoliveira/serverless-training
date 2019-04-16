8b22a3ebbb786dcd2a93add7def48aa8628d45ad
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2019-04-16 00:21:36 -0300

    init 'CreateNote' component

---
+++ src/modules/note/CreateNote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const CreateNote = () => (
+  <div>
+    <form>
+      <input type="text" placeholder="Write your note" />
+
+      <button type="sumit">Add Note</button>
+    </form>
+  </div>
+);
+
+export default CreateNote;
