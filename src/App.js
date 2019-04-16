89d5d9878150d1a2351e14434250a8b040aa759f
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2019-04-16 00:07:23 -0300

    use 'withAuthenticator' hoc to render auth flow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { withAuthenticator } from 'aws-amplify-react';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -25,4 +27,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
